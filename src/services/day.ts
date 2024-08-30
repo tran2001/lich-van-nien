a9b264b34829095442bc4650d986ffa41623c7bf
tran2001 <tran20011234@gmail.com>
2024-08-30 16:34:30 +0700

    update

--- src/services/day.ts
+++ src/services/day.ts
@@ -4,7 +4,7 @@ export const dayApi = createApi({
   reducerPath: "day",
   baseQuery: fetchBaseQuery({
     baseUrl:
-      "https://baomoi.com/_next/data/Pf59wGH4NJFwOfczrEIsO/utilities/calendar.json",
+      "https://baomoi.com/_next/data/ZsDYcNkM6Q9o3DAQwbdBE/utilities/calendar.json",
   }),
   endpoints: (builder) => ({
     getDayInfo: builder.query({
